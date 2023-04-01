8a391d9b047770481e3224a89be80b0fbcbbc1d7
Anurag <kr.anurag24@gmail.com>
2023-04-01 21:19:02 +0530

    init

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+starship init fish | source
+nvm use lts --silent
+
+alias pn="pnpm"
+alias pnd="pnpm dev"
+alias pnb="pnpm build"
+alias pns="pnpm start"
+alias pni="pnpm install"
+
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/anurag/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
